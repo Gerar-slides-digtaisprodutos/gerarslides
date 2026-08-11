4c0587ffb85823449fc14635bdc669f95f6e4569
Gerar-slides-digtaisprodutos <digtaisprodutos@gmail.com>
2026-08-11 16:32:51 +0000

    3

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
-    // !! ATENÇÃO !!
-    // Isso permite que a Vercel publique o projeto mesmo com erros de tipagem do TypeScript.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignora erros de formatação na hora de publicar
     ignoreDuringBuilds: true,
   }
 };
